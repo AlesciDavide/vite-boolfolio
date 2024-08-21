11b5fdd680e67fd7e407cb7da2799476e4fc355b
AlesciDavide <davi.alesci@Gmail.com>
2024-08-21 14:06:39 +0200

    first commit scaffolding

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script >
+import MainHeader from './components/MainHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default{
+  components:{
+    MainHeader,
+    AppMain,
+    AppFooter,
+  },
+    data() {
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template>
+  <MainHeader/>
+  <AppMain/>
+  <AppFooter/>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<script>
+export default{
+    data() {
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template>
+    <footer>
+
+        <h1>footer</h1>
+    </footer>
+</template>
+
+<style >
+
+</style>
